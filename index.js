4c33834aa9f5c3b3b4b48d27dab5a61d9a167a79
Himanshu Shekhar <hshekhar6008@gmail.com>
2023-12-15 02:05:04 +0530

    added profile pic upload and view and security improvement

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const { v4: uuidv4 } = require('uuid');
-const crypto = require('node:crypto')
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('node:crypto');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -47,6 +50,19 @@ pool.query(createTableQuery, (err) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const storage = multer.diskStorage({
+    destination: './uploads/', // Set your desired folder
+    filename: (req, file, cb) => {
+      cb(null, crypto.randomBytes(16).toString('hex') + path.extname(file.originalname));
+    },
+  });
+  
+  // Initialize multer
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 }, // Set your file size limit
+  }).single('profilePicture'); 
+
 // Passport middleware
 app.use(session({
     secret: secret, // Change this to a random secret key
@@ -56,7 +72,26 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.status(401).json({ message: 'Unauthorized' });
+  };
+
+const isAuthorized = (req, res, next) => {
+    const userId = req.params.id;
+  
+    // Check if the authenticated user has the right to access the requested user's profile picture
+    if (req.isAuthenticated() && req.user.id === userId) {
+      return next();
+    }
+  
+    res.status(403).json({ message: 'Forbidden' });
+};
 // Local Strategy
 passport.use(new LocalStrategy(
   { usernameField: 'username', passwordField: 'password' },
@@ -138,9 +173,8 @@ app.get('/logout', (req, res) => {
 });
   
   // Check if the user is authenticated
-app.get('/user/:id', (req, res) => {
+app.get('/user/:id',isAuthenticated,isAuthorized, (req, res) => {
     const id  = req.params.id;
-    if (req.isAuthenticated()) {
         pool.query('SELECT * FROM backuser WHERE id = ?', [id], (err, results) => {
             if (err) {
               return res.status(500).json({ message: 'Internal Server Error' });
@@ -154,31 +188,106 @@ app.get('/user/:id', (req, res) => {
             res.status(404).json({ message: 'User not found.' });
           }
         });
-    } else {
-    res.status(401);
-    res.json({
-        message : "Unauthorized access"
     });
-    }
+
+app.get('/userprofiledata/:id' , isAuthenticated, isAuthorized ,(req,res) => {
+        pool.query('SELECT * FROM userprofile WHERE id = ?', [id], (err, results) => {
+            if (err) {
+              return res.status(500).json({ message: 'Internal Server Error' });
+            }
+        const user = results[0];
+        if (user) {
+            // Exclude the password from the response for security
+            const { password, ...userData } = user;
+            res.json({ user: userData });
+          } else {
+            res.status(404).json({ message: 'User not found.' });
+          }
+        })
 });
 
-app.get('/userdata' , (req,res) => {
-    if(req.isAuthenticated()) {
-        const uid = req.query.id;
-        res.send("hello" + uid);
-    }
-    else{
-        res.status(401);
-        res.redirect('/logout');
-    }
-})
-app.get('*', (req,res) => {
-    res.status(404).sendFile("C:\\Users\\ASUS\\Collegeconnect\\notfound.html");
-})
 
+
+app.post('/upload/:id', isAuthenticated,isAuthorized ,(req, res) => {
+    upload(req, res, (err) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+  
+      // Check if a file was provided
+      if (!req.file) {
+        return res.status(400).json({ message: 'No file uploaded.' });
+      }
+  
+      const userId = req.params.id; // Assuming you have a user ID in the request body
+  
+      // Check if the user already has a profile picture
+      pool.query('SELECT profile_picture FROM userspic WHERE id = ?', [userId], (selectError, selectResults) => {
+        if (selectError) {
+          return res.status(500).json({ error: 'Database error.' });
+        }
+  
+        const user = selectResults[0];
+  
+        if (user && user.profile_picture) {
+            const oldFilePath = path.join(__dirname, user.profile_picture);
+        fs.unlink(oldFilePath, (unlinkError) => {
+          if (unlinkError) {
+            return res.status(500).json({ error: 'Error deleting old file.' });
+          }
+        });
+
+          // Update the existing record
+          const filePath = req.file.path;
+          pool.query('UPDATE userspic SET profile_picture = ? WHERE id = ?', [filePath, userId], (updateError) => {
+            if (updateError) {
+              return res.status(500).json({ error: 'Database error.' });
+            }
+  
+            res.json({ message: 'File updated successfully.' });
+          });
+        } else {
+          // Insert a new record
+          const filePath = req.file.path;
+          pool.query('INSERT INTO userspic (id, profile_picture) VALUES (?, ?)', [userId, filePath], (insertError) => {
+            if (insertError) {
+              return res.status(500).json({ error: 'Database error.' });
+            }
+  
+            res.json({ message: 'File uploaded successfully.' });
+          });
+        }
+      });
+    });
+  });
 
 // 
 
+app.get('/profilepicture/:id', isAuthenticated, isAuthorized ,(req, res) => {
+    const userId = req.params.id;
+  
+    // Query the database to get the profile picture path
+    pool.query('SELECT profile_picture FROM userspic WHERE id = ?', [userId], (error, results) => {
+      if (error) {
+        return res.status(500).json({ error: 'Database error.' });
+      }
+  
+      const user = results[0];
+  
+      // Check if the user and profile picture exist
+      if (!user || !user.profile_picture) {
+        return res.status(404).json({ message: 'User or profile picture not found.' });
+      }
+  
+      // Send the image data
+      const imagePath = path.join(__dirname, user.profile_picture);
+      res.sendFile(imagePath);
+    });
+  });
+
+app.get('*', (req,res) => {
+    res.status(404).sendFile("C:\\Users\\ASUS\\Collegeconnect\\notfound.html");
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
